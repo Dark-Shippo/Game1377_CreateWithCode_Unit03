18ea0b2e7e6c7419f3ec96de3f87021c39648e12
Tyler <tylerhaxton96@gmail.com>
2025-04-04 16:00:31 -0500

    Update MoveLeft.cs

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -6,6 +6,7 @@ public class MoveLeft : MonoBehaviour
     private PlayerController playerControllerScript;
     private float leftBound = -15;
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
